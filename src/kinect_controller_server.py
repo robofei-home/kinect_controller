30760e0b3ff708728e08fb950142cb7a868b3311
Bruno Spindola <bruno.spindola@outlook.com>
2021-06-17 15:55:26 -0300

    kinect_controller_server update

--- src/kinect_controller_server.py
+++ src/kinect_controller_server.py
@@ -2,14 +2,19 @@
 from kinect.srv import tilt, result
 import rospy
 
-def handle_kinect(tilt):
-    return result(tilt)
+class Kinect:
+    
+    def __init__(self, tilt):
+        self.pub=Publish(tilt)
 
-def kinect_tilt():
-    #pub = rospy.Publisher('/kinect/kinect_position_controller/command', float64, queue_size=10)
-    rospy.init_node('kinect_tilt')
-    passthrough = rospy.Service('smart_kinect', tilt, handle_kinect)
-    rospy.spin()
+    def handle_kinect(tilt):
+        return result("Success")
+
+    def kinect_tilt(self):
+        rospy.init_node('kinect_tilt')
+        passthrough = rospy.Service('smart_kinect', tilt, handle_kinect)
+        self.pub = rospy.Publisher('/kinect/kinect_position_controller/command', float64, queue_size=10)
+        rospy.spin()
 
 if __name__ == "__main__":
-    kinect_tilt_server()
+    Kinect()
